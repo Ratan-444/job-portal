265ea9b051e46345ff132999d70928bba288ac97
Ratan-444 <131332225+Ratan-444@users.noreply.github.com>
2025-09-21 16:07:05 +0530

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,11 +1,19 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('mongodb connected successfully');
-    } catch (error) {
-        console.log(error);
-    }
-}
+  if (isConnected) return;
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.error("❌ DB Connection Error:", err.message);
+  }
+};
+
 export default connectDB;
