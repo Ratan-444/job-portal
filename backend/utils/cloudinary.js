6b6886f236977e040ad9e8390694e35ca7350a5e
Ratan-444 <131332225+Ratan-444@users.noreply.github.com>
2025-09-21 16:03:41 +0530

    Update cloudinary.js

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,10 +1,13 @@
-import {v2 as cloudinary} from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 cloudinary.config({
-    cloud_name:process.env.CLOUD_NAME,
-    api_key:process.env.API_KEY,
-    api_secret:process.env.API_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+  secure: true, // optional but recommended
 });
+
 export default cloudinary;
